f77825da05b208b28cd788f829d848c0ef6b86de
DipRabadiya27 <diprabadiya@gmail.com>
2024-07-12 10:26:08 +0530

    first commit

---
+++ src/main/java/com/BiteBeat/Model/ContantInformation.java
@@ -0,0 +1,11 @@
+package com.BiteBeat.Model;
+
+import lombok.Data;
+
+@Data
+public class ContantInformation {
+    private String email;
+    private String mobile;
+    private String twitter;
+    private String instagram;
+}
